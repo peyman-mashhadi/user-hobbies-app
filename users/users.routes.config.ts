4dc36f64b0a087607df99c01eb3209feabc10398
Peyman Mashhadi <peyman@globalworkandtravel.com>
2021-08-09 15:40:03 +0200

    Start project

---
+++ users/users.routes.config.ts
@@ -0,0 +1,12 @@
+import {CommonRoutesConfig} from '../common/common.routes.config';
+import express from 'express';
+
+export class UsersRoutes extends CommonRoutesConfig {
+    constructor(app: express.Application) {
+        super(app, 'UsersRoutes');
+    }
+      configureRoutes() {
+        // (we'll add the actual route configuration here next)
+        return this.app;
+    }
+}
